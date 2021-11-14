0dd82c3a92f60032ffcc05bc063e6dec6bbe0770
Marcelo <marcelosj7159@gmail.com>
2021-11-14 14:11:38 -0300

    Adicionando funcionalidade do botão excluir

--- app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -17,8 +17,8 @@ class FornecedorController extends Controller
         ->where('site','like','%'.$request->input('site').'%')
         ->where('uf','like','%'.$request->input('uf').'%')
         ->where('email','like','%'.$request->input('email').'%')
-        ->get();
-        return view('app.fornecedor.listar',['fornecedores'=> $fornecedores]);
+        ->paginate(2);
+        return view('app.fornecedor.listar',['fornecedores'=> $fornecedores,'request' => $request->all()]);
     }
 
     public function adicionar(Request $request){
@@ -54,18 +54,26 @@ class FornecedorController extends Controller
             if($update){
                 
                 $msg="Registro atualizado com sucesso";
+
+               
             }else{
                 $msg="Falha ao atualizar registro";
             }
+            return redirect()->route('app.fornecedor.editar',[ 'id' => $request->input('id') ,'msg'=> $msg]);
         }
 
         return view('app.fornecedor.adicionar',['msg'=> $msg]);
     }
 
-    public function editar($id){
+    public function editar($id,$msg = ''){
         
         $fornecedor = Fornecedor::find($id);
 
-        return view("app.fornecedor.adicionar",['fornecedor'=> $fornecedor]);
+        return view("app.fornecedor.adicionar",['fornecedor'=> $fornecedor,'msg'=> $msg]);
+    }
+
+    public function excluir($id){
+        Fornecedor::find($id)->delete();
+        return redirect()->route("app.fornecedor");
     }
 }
