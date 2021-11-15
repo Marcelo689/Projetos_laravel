3a8ddeb490505ca47887e1021daf575465ffc96e
Marcelo <marcelosj7159@gmail.com>
2021-11-15 06:58:22 -0300

    Adicionando model produto e unidade

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -29,16 +29,17 @@ Route::middleware('autenticacao:padrao,Marcelo')->prefix('/app')->group(function
     Route::get('/sair', 'LoginController@sair')->name('app.sair');
     Route::get('/cliente','ClienteController@index')->name('app.cliente');
     Route::get('/fornecedor', 'FornecedorController@index')->name('app.fornecedor');
+
     Route::get('/fornecedor/editar/{id}/{msg?}','FornecedorController@editar')->name('app.fornecedor.editar');
     Route::get('/fornecedor/excluir/{id}','FornecedorController@excluir')->name('app.fornecedor.excluir');
+
     Route::get('/fornecedor/adicionar', 'FornecedorController@adicionar')->name('app.fornecedor.adicionar');
     Route::post('/fornecedor/adicionar', 'FornecedorController@adicionar')->name('app.fornecedor.adicionar');
 
     Route::post('/fornecedor/listar', 'FornecedorController@listar')->name('app.fornecedor.listar');    
     Route::get('/fornecedor/listar', 'FornecedorController@listar')->name('app.fornecedor.listar');    
 
-    
-    Route::get('/produto','ProdutoController@index')->name('app.produto');
+    Route::resource('produto','ProdutoController');
 });
 
 Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('site.teste');
