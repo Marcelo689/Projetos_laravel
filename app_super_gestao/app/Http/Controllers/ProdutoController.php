7508c8631e46373b4f89170a2f81b740d508dc0b
Marcelo <marcelosj7159@gmail.com>
2021-11-16 12:20:52 -0300

    Adicionando botão delete

--- app_super_gestao/app/Http/Controllers/ProdutoController.php
+++ app_super_gestao/app/Http/Controllers/ProdutoController.php
@@ -98,6 +98,7 @@ class ProdutoController extends Controller
      */
     public function destroy(Produto $produto)
     {
-        //
+        $produto->delete();
+        return redirect()->route('produto.index');
     }
 }
