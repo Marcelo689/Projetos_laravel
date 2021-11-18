c0560b97cd88fc9224d3b95af31aa441f7557296
Marcelo <marcelosj7159@gmail.com>
2021-11-18 12:11:07 -0300

    Substituindo Lazy loading por eager loading no produtoController index

--- app_super_gestao/resources/views/app/produto/index.blade.php
+++ app_super_gestao/resources/views/app/produto/index.blade.php
@@ -17,7 +17,7 @@
 
         <div class="informacao-pagina">
                 <div style="width: 90%;margin-left:auto;margin-right:auto">
-                    
+                 
                         <table border="1" width='100%'>
                             <thead>
                                     <th>Nome</th>
